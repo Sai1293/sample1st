fa691262309fc174b6ae0fd14b07bef7e2df8a06
Sai1293 <neldurga.saikiran1293@gmail.com>
2023-01-13 15:53:14 +0530

    Second Commit

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,8 +1,8 @@
-public class HelloWorld {
+public class HelloJenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello Jenkins");
 
 	}
 
--- HelloWipro.java
+++ HelloWipro.java
@@ -1,8 +1,8 @@
-public class HelloWorld {
+public class HelloWipro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello Wipro");
 
 	}
 
